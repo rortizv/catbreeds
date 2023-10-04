b0004cb16cac85750e8d4b62883fbe8526063f26
Rafael Ortiz <rortiz@inkremental.co>
2023-10-03 21:55:17 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'catbreeds',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
